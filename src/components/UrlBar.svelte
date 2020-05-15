59042f255fa7e286aa9095646d7b4f2ed72c0c88
Etienne Burdet <etienne.burdet@gmail.com>
2020-05-15 15:25:46 +0200

    get records from push url

--- src/components/UrlBar.svelte
+++ src/components/UrlBar.svelte
@@ -1,5 +1,7 @@
 <script>
-let url
+
+let baseUrl
+let pushUrl
 
 const findId = (url) => {
   const datasetRegexp = /api\/push\/1\.0\/([a-zA-Z0-9_-]*)(?=\/realtime)/
@@ -7,18 +9,21 @@ const findId = (url) => {
   return datasetId
 }
 
+const getSearchUrl= (datasetId) => baseUrl + '/api/v2/catalog/datasets/' + datasetId + '/records'
+
 const fetchData = async (event) => {
   event.preventDefault()
-  const datasetId = findId(url)
-  console.log(datasetId);
-  // const res = await fetch(url)
-  // const data = await res.json()
-  // const data
+  const datasetId = findId(pushUrl)
+  const searchUrl = getSearchUrl(datasetId)
+  const res = await fetch(searchUrl)
+  const data = await res.json()
+  console.log(data)
 }
 </script>
 
 <form action="#" on:submit={fetchData}>
-  <input type="text" name="url" bind:value={url}>
+  <input type="text" name="url" bind:value={baseUrl} placeholder="Paste base domain URL">
+  <input type="text" name="url" bind:value={pushUrl} placeholder="Paste push URL with key">
   <input type="submit" value="Rechercher">
 </form>
 
