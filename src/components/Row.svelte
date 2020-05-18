98b3c1301af8b4b91405fc7e54ddace58574a396
Etienne Burdet <etienne.burdet@gmail.com>
2020-05-18 18:10:12 +0200

    added node modules from gitignore

--- src/components/Row.svelte
+++ src/components/Row.svelte
@@ -1,9 +1,32 @@
 <script>
+  import Cell from './Cell.svelte'
   export let values
 </script>
 
 <tr>
+  <td class="delete-button">
+    <img src="../img/trash-2.svg" alt="trash" width="14px">
+  </td>
   {#each values as value}
-    <td>{value}</td>
+    <Cell value={value} />
   {/each}
 </tr>
+
+<style>
+  tr:hover {
+    background: rgb(148, 206, 231, 0.2);
+  }
+
+  .delete-button {
+    visibility: hidden;
+    border: none;
+  }
+
+  .delete-button:hover {
+    background: rgb(148, 206, 231, 0.8);
+  }
+
+  tr:hover .delete-button {
+    visibility: visible;
+  }
+</style>
--- src/components/Table.svelte
+++ src/components/Table.svelte
@@ -2,20 +2,27 @@
 import { records } from '../store.js'
 import Row from './Row.svelte'
 
-let headers = []
-let rows = []
+let headers = ['Nom', 'Commentaire', 'Note']
+let rows = [
+  ['Berberhygge', 'couscous dannois', 5],
+  ['Kleen Canteen', 'hipsters', 4]
+]
 
-$: {
-  if($records) {
-    headers = Object.keys($records[0].record.fields)
-    rows = $records.map(record => Object.values(record.record.fields))
-  }
-}
+// $: {
+//   if($records) {
+//     headers = Object.keys($records[0].record.fields)
+//     rows = $records.map(record => Object.values(record.record.fields))
+//   }
+// }
+
+
 </script>
 
 <table>
+  <col span="1" class="delete-col">
   <thead>
     <tr>
+      <th></th>
       {#each headers as header}
         <th>{header}</th>
       {/each}
@@ -27,3 +34,16 @@ $: {
     {/each}
   </tbody>
 </table>
+
+<style>
+  table {
+    table-layout: fixed;
+    width: 100%;
+    text-align: center;
+    border-collapse: collapse;
+  }
+
+  .delete-col {
+    width: 32px;
+  }
+</style>
