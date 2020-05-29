9c1129e5e98e23c09156022ecef1d3493f94cda2
Etienne Burdet <etienne.burdet@gmail.com>
2020-05-29 09:32:32 +0200

    hardcode url in bar

--- src/components/UrlBar.svelte
+++ src/components/UrlBar.svelte
@@ -2,17 +2,16 @@
 import { getData } from '../helpers/odsData.js'
 import { rows, headers } from '../store.js'
 
-let typedUrl
+const url = 'https://eburdet.opendatasoft.com/api/v2/catalog/datasets/restaurants/records' //hardcode for test
 
 const getLatestData = async () => {
-  const url = 'https://eburdet.opendatasoft.com/api/v2/catalog/datasets/restaurants/records' //hardcode for test
   $rows = await getData(url)
   $headers = Object.keys($rows[0])
 }
 </script>
 
 <form action="#" on:submit|preventDefault={getLatestData}>
-  <input type="text" name="url" bind:value={typedUrl} placeholder="Paste dataset Url">
+  <input type="text" name="url" value={url} placeholder="Paste dataset Url">
   <input type="submit" value="Get latest data">
 </form>
 
